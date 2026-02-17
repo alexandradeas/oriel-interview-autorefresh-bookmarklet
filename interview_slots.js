7e9b66cff8dbbb0ed23b15768fbd4bb7ed99bad5
Alexandra Deas <me@alexandradeas.co.uk>
2026-02-17 10:30:22 +0000

    only run on oriel application page

--- interview_slots.js
+++ interview_slots.js
@@ -1,17 +1,22 @@
 'use strict';
 (function() {
-	document.addEventListener('DOMContentLoaded', function() {
-		const delay = 5000; // ms
-		const beepFile = "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
+	const url = "https://www.oriel.nhs.uk/Web/Applicant/Application"
+	const delay = 5000; // ms
+	const beepFile = "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
+
+	if (window.location.href === url) {
 		const beep = new Audio(beepFile);
-		setTimeout(function() {
-			const noData = document.querySelector("#noDataMessage").checkVisibility();
-			if (noData) {
-				window.location.reload();
-			} else {
-				beep.play();
-				alert("Interview slots available");
-			}
-		}, delay);
-	})
+
+		document.addEventListener('DOMContentLoaded', function() {
+			setTimeout(function() {
+				const noData = document.querySelector("#noDataMessage").checkVisibility();
+				if (noData) {
+					window.location.reload();
+				} else {
+					beep.play();
+					alert("Interview slots available");
+				}
+			}, delay);
+		})
+	}
 })()
--- shortlist.js
+++ shortlist.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 (function() {
-	document.addEventListener('DOMContentLoaded', function() {
-		const delay = 5000; // ms
-		const beepFile = "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
-		const beep = new Audio(beepFile);
-		setTimeout(function() {
-			const shortlistBlock = document.getElementById("stageShortlist");
-			const inProgress = shortlistBlock.querySelector(".in-progress");
-			const passed = shortlistBlock.querySelector(".passed");
-			if (inProgress) {
-				window.location.reload();
-			} else if (passed) {
-				beep.play();
-				alert("Interview slots available");
-			} else {
-				beep.play();
-				alert("Unknown shortlist status");
-			}
-		}, delay);
-	})
-})();
+	const url = "https://www.oriel.nhs.uk/Web/Applicant/Application";
+	const delay = 5000; // ms
+	const beepFile =
+		"https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
 
+	if (window.location.href === url) {
+		const beep = new Audio(beepFile);
+
+		document.addEventListener("DOMContentLoaded", function() {
+			setTimeout(function() {
+				const shortlistBlock = document.getElementById("stageShortlist");
+				const inProgress = shortlistBlock.querySelector(".in-progress");
+				const passed = shortlistBlock.querySelector(".passed");
+				if (inProgress) {
+					window.location.reload();
+				} else if (passed) {
+					beep.play();
+					alert("Interview slots available");
+				} else {
+					beep.play();
+					alert("Unknown shortlist status");
+				}
+			}, delay);
+		});
+	}
+})();
