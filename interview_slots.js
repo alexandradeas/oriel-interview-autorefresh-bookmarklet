e71b3fc8bc3daabafabb01b0c8fac1791695ea98
Alexandra Deas <me@alexandradeas.co.uk>
2026-02-17 10:50:52 +0000

    adapt scripts to work with tampermonkey

--- interview_slots.js
+++ interview_slots.js
@@ -1,22 +1,26 @@
-'use strict';
+// ==UserScript==
+// @name         Interview Slots
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Run my script on page load
+// @match        https://www.oriel.nhs.uk/Web/Application/Interview
+// @grant        none
+// ==/UserScript==
+
 (function() {
-	const url = "https://www.oriel.nhs.uk/Web/Applicant/Application"
-	const delay = 5000; // ms
+	"use strict";
+	const delay = 60000; // 1m
 	const beepFile = "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
 
-	if (window.location.href === url) {
-		const beep = new Audio(beepFile);
+	const beep = new Audio(beepFile);
 
-		document.addEventListener('DOMContentLoaded', function() {
-			setTimeout(function() {
-				const noData = document.querySelector("#noDataMessage").checkVisibility();
-				if (noData) {
-					window.location.reload();
-				} else {
-					beep.play();
-					alert("Interview slots available");
-				}
-			}, delay);
-		})
-	}
+	setTimeout(function() {
+		const noData = document.querySelector("#noDataMessage").checkVisibility();
+		if (noData) {
+			window.location.reload();
+		} else {
+			beep.play();
+			alert("Interview slots available");
+		}
+	}, delay);
 })()
--- shortlist.js
+++ shortlist.js
@@ -1,28 +1,32 @@
-"use strict";
+// ==UserScript==
+// @name         Shortlist
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Alexandra Deas
+// @description  Auto-refreshes the oriel page and plays an audible and alert on status change
+// @match        https://www.oriel.nhs.uk/Web/Application/Application
+// @grant        none
+// ==/UserScript==
+
 (function() {
-	const url = "https://www.oriel.nhs.uk/Web/Applicant/Application";
-	const delay = 5000; // ms
+	const delay = 60000; // 1m
 	const beepFile =
 		"https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
 
-	if (window.location.href === url) {
-		const beep = new Audio(beepFile);
+	const beep = new Audio(beepFile);
 
-		document.addEventListener("DOMContentLoaded", function() {
-			setTimeout(function() {
-				const shortlistBlock = document.getElementById("stageShortlist");
-				const inProgress = shortlistBlock.querySelector(".in-progress");
-				const passed = shortlistBlock.querySelector(".passed");
-				if (inProgress) {
-					window.location.reload();
-				} else if (passed) {
-					beep.play();
-					alert("Interview slots available");
-				} else {
-					beep.play();
-					alert("Unknown shortlist status");
-				}
-			}, delay);
-		});
-	}
+	setTimeout(function() {
+		const shortlistBlock = document.getElementById("stageShortlist");
+		const inProgress = shortlistBlock.querySelector(".in-progress");
+		const passed = shortlistBlock.querySelector(".passed");
+		if (inProgress) {
+			window.location.reload();
+		} else if (passed) {
+			beep.play();
+			alert("Interview slots available");
+		} else {
+			beep.play();
+			alert("Unknown shortlist status");
+		}
+	}, delay);
 })();
